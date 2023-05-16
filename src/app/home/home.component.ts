fa940c1d9abddd36b50e3c25b54ab25054b8c21f
Thong Tang <tmthong@tma.com.vn>
2023-05-16 10:02:40 +0700

    ngFor with table

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,10 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   public name = 'Hien';
   public age = 15;
+  public fruits = ['Apple', 'Orange', 'Tomato'];
+  public fruits2 = [{ name: 'Apple', price: 10, isSale: true },
+  { name: 'Orange', price: -1, isSale: false },
+  { name: 'Tomato', price: 12.21, isSale: true }];
   public resetName(): void {
     console.log('reset name function is called')
     this.name = '';
