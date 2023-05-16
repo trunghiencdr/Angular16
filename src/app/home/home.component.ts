01bed25f5631ccfa3c6fce0f394b357ec29520a7
Thong Tang <tmthong@tma.com.vn>
2023-05-16 14:14:10 +0700

    add combobox choose city and district

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,12 +8,29 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   public name = 'Hien';
   public age = 15;
-  public fruits = ['Apple', 'Orange', 'Tomato'];
-  public fruits2 = [{ name: 'Apple', price: 10, isSale: true },
-  { name: 'Orange', price: -1, isSale: false },
-  { name: 'Tomato', price: 12.21, isSale: true }];
+  public districts: string[] = ['Choose district']
+  public datas = [
+    {
+      city: 'Choose city'
+    }
+    ,{
+    city: 'HCM',
+    districts: ['District 1', 'District 2', 'District 3']
+  }, {
+    city: 'Tien Giang',
+    districts: ['Cai Be', 'My Tho', 'Cai Lay', 'Chau Thanh']
+  }];
   public resetName(): void {
     console.log('reset name function is called')
     this.name = '';
+  }
+
+  public cityChanged(event: any): void {
+    console.log('city changed:', event.target.value)
+    const city = event.target.value
+    if(!city){
+      return
+    }
+    this.districts = this.datas.find(data => data.city === city )?.districts || ['Choose district']
   }
 }
