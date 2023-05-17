ef34d37ceb0ce6bf500e8cd29e7ebbaa455bece7
Thong Tang <tmthong@tma.com.vn>
2023-05-17 10:07:44 +0700

    .

---
+++ src/app/Services/common.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommonService {
+
+  constructor() { }
+
+  private counter = 0;
+  setCounter(counter: number) {
+    this.counter = counter
+  }
+  getCounter(): number {
+    return this.counter
+  }
+
+  square(number: number): number {
+    return number * number
+  }
+
+}
--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonService } from '../Services/common.service';
 
 @Component({
   selector: 'app-about',
@@ -8,4 +9,13 @@ import { Component } from '@angular/core';
 export class AboutComponent {
   public username = 'admin';
   public color = 'pink'
+  public counter = 0
+  constructor(private common: CommonService){
+    this.counter = common.getCounter()
+    common.setCounter(this.counter+1)
+  }
+
+  squareCounter(number: number){
+    return this.common.square(number)
+  }
 }
